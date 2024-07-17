06c6e3f5eda6d3664f11e84139b54a068e140747
kidusshoa <captainkevin2008@gmail.com>
2024-07-17 15:20:50 +0300

    fronted : part 8

--- front-end/src/components/Catagories/Region/FilterCard.tsx
+++ front-end/src/components/Catagories/Region/FilterCard.tsx
@@ -14,7 +14,7 @@ const FilterComponent = () => {
   return (
     <>
       <div className="w-full h-full p-5 flex flex-row-reverse">
-        <div className="w-full md:w-1/4 md:h-40 rounded h-full md:scale-150 flex flex-row md:gap-4 gap-2 items-center justify-center ">
+        <div className=" h-full rounded md:scale-150 flex flex-row px-3 items-center justify-center ">
           <button
             onClick={openModal}
             className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 focus:outline-none focus:bg-green-600"
@@ -48,7 +48,7 @@ const FilterComponent = () => {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left  shadow-xl transform transition-all sm:my-8 sm:align-middle  md:w-1/2 sm:w-full sm:p-6">
+                <Dialog.Panel className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left  shadow-xl transform transition-all sm:my-8 sm:align-middle  md:w-1/4 sm:w-full sm:p-6">
                   <div className="sm:flex md:w-full sm:items-start">
                     <div className="mt-3 text-center md:w-full sm:mt-0 sm:ml-4 sm:text-left">
                       <Dialog.Title
@@ -58,8 +58,8 @@ const FilterComponent = () => {
                         Filter Properties
                       </Dialog.Title>
                       <div className="mt-2 w-full">
-                        <div className="mt-5 w-full sm:mt-6 sm:flex flex gap-2 ">
-                          <div className="space-y-2 w-1/3 gap-3 flex flex-col ">
+                        <div className="mt-5 w-full sm:mt-6 sm:flex flex-col gap-2 ">
+                          <div className="space-y-2 w-full gap-3 flex flex-col ">
                             <label
                               htmlFor="categories"
                               className="block text-gray-700 text-sm font-bold"
@@ -80,7 +80,7 @@ const FilterComponent = () => {
 
                             <p className="text-red-500 text-sm"></p>
                           </div>
-                          <div className="space-y-2 w-1/3 gap-3 flex flex-col ">
+                          <div className="space-y-2 w-full gap-3 flex flex-col ">
                             <label
                               htmlFor="city"
                               className="block text-gray-700 text-sm font-bold"
@@ -101,12 +101,12 @@ const FilterComponent = () => {
 
                             <p className="text-red-500 text-sm"></p>
                           </div>
-                          <div className="w-1/3 h-full flex justify-around">
+                          <div className="w-full h-full flex flex-row-reverse gap-2">
                             <button
                               type="submit"
                               className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
                             >
-                              Search
+                              Apply
                             </button>
                             <button
                               type="button"
